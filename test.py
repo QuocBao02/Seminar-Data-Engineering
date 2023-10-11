10abc3d5f65298a670b2b98c94ec903a5db0dd29
QuocBao02 <82230028+QuocBao02@users.noreply.github.com>
2023-10-11 08:40:02 +0700

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName("Read_data").getOrCreate()
-data=spark.read.parquet("/home/quocbao/Downloads/data.parquet")
-
-data.show()
-
-spark.stop()
